5363dffe6c685a1034c9635f05e7abae5e5fb8cd
Valentina Ronchi <valentinaronchi@Valentinas-MacBook-Pro.local>
2020-07-18 20:19:13 +0200

    commit

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/question5/login', function (req, res) {
     //     password:password,
     //     email:email
     // }
-     let token = jwt.sign(data, process.env.TOKEN_SECRET)
+     let token = jwt.sign({data}, process.env.TOKEN_SECRET)
     // if (token) {
         // let result = insertIntoTable(name, email, token)
         res.json({ result: token });
