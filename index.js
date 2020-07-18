d7bebf64960ae5f64193b61e09cd9690303ee79b
Valentina Ronchi <valentinaronchi@Valentinas-MacBook-Pro.local>
2020-07-18 03:05:25 +0200

    commit

--- index.js
+++ index.js
@@ -107,10 +107,10 @@ const getFromTable = (email) => {
 }
 
 
-app.get('/question6/login', function (req, res) {
+app.get('/question6/login', function async(req, res) {
     // connect to db, check for email, get token
     const { password, email } = req.body
-    let token = getFromTable(email)
+    let token = await getFromTable(email)
     if(token){
         jwt.verify(token, password, function (err, data) {
             if (err) {
