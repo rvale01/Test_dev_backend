3b648560706c140f648a64e676c1b4ed2d340103
Valentina Ronchi <valentinaronchi@Valentinas-MacBook-Pro.local>
2020-07-19 00:40:54 +0200

    commit

--- index.js
+++ index.js
@@ -118,15 +118,17 @@ app.post('/question5/login', async function (req, res) {
     } else if (checking === "not exist") {
         let token = jwt.sign({ data }, privateKEY, signOptions)
         if (token) {
-            res.json({ "result": token });
             // res.json({ result: 'call ok!' });
-            // insertIntoTable(name, email, token)
-            //     .then((result) => {
-            //         res.json({ "result": result });
-            //     })
+            insertIntoTable(name, email, token)
+                .then((result) => {
+                    res.json({ result: result });
+                })
+                .catch((err)=>{
+                    res.json({"ops":err})
+                })
             // res.json({ result: "test" });
         } else {
-            res.json({ "result": "test" });
+            res.json({ "result": result });
         }
     }
 })
